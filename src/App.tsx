b786c6f1c149e27a419f9cdb2ba72e05c2aede9b
PARSIVALJ <uchejesusboy@gmail.com>
2024-02-08 12:37:29 +0200

    installing mui icons adding navbar components adding items to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
 
-
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-
+      <Navbar />
     </div>
   );
 }
