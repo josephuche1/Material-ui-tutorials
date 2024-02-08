26c5a6aa2890a1fc844bceada4df78dfbbecdebb
PARSIVALJ <uchejesusboy@gmail.com>
2024-02-08 13:23:45 +0200

    react-router-dom setup

---
+++ src/pages/Authentication/Authentication.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Authentication = () => {
+  return (
+    <div style={{marginLeft: "500px"}}>
+      this is the authentication page
+    </div>
+  )
+}
+
+export default Authentication
