83eba3f9412c9e9ba9a240845adee7d4734d729c
PARSIVALJ <uchejesusboy@gmail.com>
2024-02-08 17:10:09 +0200

    Add style and add more button

--- src/pages/Authentication/Authentication.tsx
+++ src/pages/Authentication/Authentication.tsx
@@ -3,24 +3,43 @@ import Grid from '@mui/material/Grid';
 import CommonButton from '../../components/common/CommonButton/CommonButton';
 
 const Authentication = () => {
-  const buttonStyles= {
-    fontSize: 20,
-    fontWeight: 200,
-    backgroundColor: "tomato",
-    "&:hover": {
-      backgroundColor: "black",
-      color: "tomato"
-    }
-  }
+  const buttonStyles = {
+    fontSize: '0.875rem',
+    fontWeight: 600,
+    textTransform: 'capitalize',
+    borderRadius: 2.5,
+    '&.MuiButton-contained': {
+        backgroundColor: '#009be5',
+        '&:hover': {
+            backgroundColor: '#006db3'
+        },
+    },
+    '&.MuiButton-outlined': {
+        color: "#fff",
+        borderColor: '#fff',
+        '&:hover': {
+            backgroundColor: 'transparent',
+            borderColor: "green",
+            color: "green"
+        },
+    },
+};
 
   return (
     <Grid xs={8}>
       this is the authentication page
       <CommonButton
-        size="small"
         variant="contained"
         sx={buttonStyles}
-      > Add User </CommonButton>
+      > 
+        Add User 
+      </CommonButton>
+      <CommonButton
+        variant="outlined"
+        sx={buttonStyles}
+      > 
+        Web Setup
+      </CommonButton>
     </Grid>
   )
 }
