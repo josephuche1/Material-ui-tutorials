47f006ecc828353e4ac73ff1daae9785160f22af
PARSIVALJ <uchejesusboy@gmail.com>
2024-02-10 00:44:20 +0200

    Linking functionality between Menu and Notification

--- src/components/common/NotificationBell/NotificationBell.tsx
+++ src/components/common/NotificationBell/NotificationBell.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Badge from '@mui/material/Badge';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import IconButton  from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
+import BasicMenu from '../BasicMenu/BasicMenu';
 
 interface INotificationBellProps {
     color?: "inherit" | "primary" | "secondary" | "success" | "error" | "info" | "warning",
@@ -13,20 +14,38 @@ interface INotificationBellProps {
 const NotificationBell: React.FC<INotificationBellProps> = (props) => {
   const notifications = `You have ${props.badgeContent} notifications`;
   const noNotifications = "You have no notifications";
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = (event: React.MouseEvent<HTMLButtonElement>): void => {
+    setAnchorEl(event.currentTarget);
+    setOpen(true);
+  };
+
+  const [open, setOpen] = useState<boolean>(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
   return (
-    <Tooltip title={props.badgeContent && props.badgeContent !== 0 ? notifications : noNotifications }>
-        <IconButton 
-            color={props.color}
-            sx={props.sx}
-        >
-            <Badge 
-                badgeContent={props.badgeContent} 
-                color="error"
+    <div>
+        <Tooltip title={props.badgeContent && props.badgeContent !== 0 ? notifications : noNotifications }>
+            <IconButton 
+                color={props.color}
+                onClick={handleOpen}
             >
-                <NotificationsIcon />
-            </Badge>
-        </IconButton>
-    </Tooltip>
+                <Badge 
+                    badgeContent={props.badgeContent} 
+                    color="error"
+                >
+                    <NotificationsIcon />
+                </Badge>
+            </IconButton>
+        </Tooltip>
+        <BasicMenu
+            anchorEl={anchorEl}
+            open={open}
+            handleClose={handleClose}
+        />
+    </div>
   )
 }
 
--- src/pages/Authentication/Authentication.tsx
+++ src/pages/Authentication/Authentication.tsx
@@ -1,37 +1,17 @@
 import React from 'react'
 import Grid from '@mui/material/Grid';
-import CommonButton from '../../components/common/CommonButton/CommonButton';
 import NotificationBell from '../../components/common/NotificationBell/NotificationBell';
-import  BasicMenu from '../../components/common/BasicMenu/BasicMenu';
 
-const Authentication = () => {
-  const buttonStyles = {
-    fontSize: '0.875rem',
-    fontWeight: 600,
-    textTransform: 'capitalize',
-    borderRadius: 2.5,
-    '&.MuiButton-contained': { 
-        backgroundColor: '#009be5',
-        '&:hover': {
-            backgroundColor: '#006db3'
-        },
-    },
-    '&.MuiButton-outlined': {
-        color: "#fff",
-        borderColor: '#fff',
-        '&:hover': {
-            backgroundColor: 'transparent',
-            borderColor: "green",
-            color: "green"
-        },
-    },
-  };
+const Authentication:React.FC = () => {
+
 
   return (
     <Grid xs={8} sx={{backgroundColor: "cyan"}}>
         This is the Authentication page
-        <NotificationBell badgeContent={5} />
-        <BasicMenu />
+        <NotificationBell 
+          badgeContent={5}
+          color="primary"
+        />
     </Grid>
   )
 }
