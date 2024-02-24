887cb67cac48606c823816d91b5a3051ebee9753
PARSIVALJ <uchejesusboy@gmail.com>
2024-02-24 20:39:25 +0200

    Avata component

--- src/pages/Authentication/Authentication.tsx
+++ src/pages/Authentication/Authentication.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Grid from '@mui/material/Grid';
 import NotificationBell from '../../components/common/NotificationBell/NotificationBell';
 import CommonButton from '../../components/common/CommonButton/CommonButton';
+import Avatar from '@mui/material/Avatar';
 
 const Authentication:React.FC = () => {
   return (
@@ -12,6 +13,9 @@ const Authentication:React.FC = () => {
         </CommonButton>
         <NotificationBell 
           color="primary"
+        />
+        <Avatar 
+          src="https://mui.com/static/images/avatar/1.jpg"
         />
     </Grid>
   )
