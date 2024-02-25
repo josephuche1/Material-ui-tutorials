51d7b3c004a60f3711191567b45fee51c77daf49
PARSIVALJ <uchejesusboy@gmail.com>
2024-02-25 16:09:22 +0200

    Adding conditional logic to display different title for different URL

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Grid from '@mui/material/Grid';
 import Header from './components/common/Header/Header';
 
 const App: React.FC = () => {
+  const [title, setTitle] = useState<string>("");
+  const location = useLocation();
+
+  useEffect(() => {
+    const parseLocation = location.pathname.replace("/", "");
+    setTitle(parseLocation);
+  }, [location])
 
   return (
     <Grid container>
       <Navbar />
-      <Header title="Authentication" />
+      <Header 
+        title={title} />
       <Outlet />  { /*The outlet element is a placeholder for the child route's content.*/}
     </Grid>
   );
