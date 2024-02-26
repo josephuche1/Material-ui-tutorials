146a830d6a3df0064ebebedd10f12bdf473b764f
PARSIVALJ <uchejesusboy@gmail.com>
2024-02-26 13:42:30 +0200

    Card

---
+++ src/components/common/BasicCard/BasicCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+
+const BasicCard: React.FC = () => {
+  return (
+    <Card>
+        <CardContent>
+            Card Content
+        </CardContent>
+    </Card>
+  )
+}
+
+export default BasicCard
--- src/pages/Authentication/Authentication.tsx
+++ src/pages/Authentication/Authentication.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Grid from '@mui/material/Grid';
-import Header from '../../components/common/Header/Header'
+import Header from '../../components/common/Header/Header';
+import BasicCard from '../../components/common/BasicCard/BasicCard';
 
 const Authentication:React.FC = () => {
   return (
-    <Grid item xs={8}>
- 
+    <Grid item xs={8} sx={{marginLeft:"320px"}}>
+      <BasicCard />
     </Grid>
   )
 }
