0997d16889610d0f51261075fff105ddd1ce0e2a
PARSIVALJ <uchejesusboy@gmail.com>
2024-02-29 15:00:24 +0200

    install dependencies for vlidating user input

--- src/components/Modals/NewUserModal/NewUserModal.tsx
+++ src/components/Modals/NewUserModal/NewUserModal.tsx
@@ -2,14 +2,30 @@ import React from 'react'
 import BasicModal from '../../common/BasicModal/BasicModal';
 import { TBasicModalProps } from '../../common/Types/Modaltype';
 import Box from '@mui/material/Box';
-import Input from '@mui/material/Input';
 import { modalStyles } from '../../common/BasicModal/styles';
 import CommonButton from '../../common/CommonButton/CommonButton';
 import TextField from '@mui/material/TextField';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 
 
 const NewUserModal:React.FC<TBasicModalProps> = (props) => {
+ const validationSchema = Yup.object().shape({
+    name: Yup.string().required('Name is required'),
+    email: Yup.string().email('Email is invalid').required('Email is required'),
+    phone: Yup.string()
+     // .matches(phoneRegex, "Phone number is not valid")
+ });
+
+  const {
+    register, 
+    handleSubmit, 
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(validationSchema)
+  });
   const validate = (): void => {
     console.log('Validating')
   }
